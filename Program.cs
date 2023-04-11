fc27129afd844273c452420ed360aca976838ab8
OlegGutsko <128018340+OlegGutsko@users.noreply.github.com>
2023-04-11 20:57:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_PolesUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Вывести на консоль
+            Console.WriteLine(" +\"\"\"\"\"+");
+            Console.WriteLine("(| o o |)");
+            Console.WriteLine(" |  ^  |");
+            Console.WriteLine(" | '-' |");
+            Console.WriteLine(" +-----+");
+
+            Console.ReadLine();
+        }
+    }
+}
